a60f7afcb6f7386f3e6af8578b7d5864a4a6f20e
sajimathew <smathew@alpha-sense.com>
2019-10-04 15:23:13 -0400

    Corrected the column name.

--- models/EntityAdditionModel.js
+++ models/EntityAdditionModel.js
@@ -1,8 +1,8 @@
 
 function EntityAddition(values) {
       this.entityUID = values['ENTITY_UID']
-      this.key = values['PROPS']
-      this.vale = values['PROP_VALUE']
+      this.key = values['PROP']
+      this.value = values['PROP_VALUE']
         console.log(values)
    }
 
